794773c772ae82e87127a689864b250b31bc87a6
Attrixx <attrix1221@gmail.com>
2025-02-04 10:50:25 +0100

    Update User.h

--- NetworkCommon/include/User.h
+++ NetworkCommon/include/User.h
@@ -6,17 +6,15 @@
 class User
 {
 public:
-	User(int id, const std::string& name, const std::string& address, u_short port) : m_id(id), m_name(name), m_address(address), m_port(port) {}
+	User(int id, const std::string& name, u_short port) : m_id(id), m_name(name), m_port(port) {}
 
 	int GetId() const { return m_id; }
 	const std::string& GetName() const { return m_name; }
-	const std::string& GetAddress() const { return m_address; }
 	u_short GetPort() const { return m_port; }
 
 private:
 	int m_id;
 	std::string m_name;
-	std::string m_address;
 	u_short m_port;
 };
 
