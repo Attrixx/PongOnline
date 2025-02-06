95775dc25ce3dd33ff9c611ac29b4f3b3d3b4437
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2025-02-06 10:55:51 +0100

    [ADD] lobbies buttons

--- Client/src/LobbyListScene.cpp
+++ Client/src/LobbyListScene.cpp
@@ -1,9 +1,28 @@
 #include "LobbyListScene.h"
+#include "CommonGameConsts.h"
+#include "ClientGameConsts.h"
+#include "Message.h"
+#include "Button.h"
+#include "ClientApp.h"
 
 #include <iostream>
 
 void LobbyListScene::InitLobbiesList(std::vector<LobbyStruct> lobbies)
 {
+	for (int i = 0; i < lobbies.size(); i++)
+	{
+		Button* lobbyButton = CreateUIElement<Button>();
+		lobbyButton->SetPosition({ float(GetScreenWidth() / 2), (BUTTON_HEIGHT * 1.5f) * i });
+		lobbyButton->SetText(lobbies[i].name + " " + std::to_string(lobbies[i].currentSize) + "/" + std::to_string(lobbies[i].maxSize));
+		auto onLobbyButtonClicked = [&]() {
+			Message message = Message::CreateMessage(MessageType::JOIN_LOBBY, {
+				{"lobbyId", lobbies[i].id}
+				});
+			message.content["id"] = I(ClientApp)->GetClientId();
+			I(ClientApp)->SendMessage(message);
+			};
+		lobbyButton->BindOnClickFunction(onLobbyButtonClicked);
+	}
 }
 
 void LobbyListScene::OnInitialize()
