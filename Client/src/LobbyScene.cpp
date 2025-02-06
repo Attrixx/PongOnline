598f12afd505e3206f73cb51dc305100d4019da5
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2025-02-06 12:46:49 +0100

    [ADD] lobby buttons

--- Client/src/LobbyScene.cpp
+++ Client/src/LobbyScene.cpp
@@ -1,7 +1,32 @@
 #include "LobbyScene.h"
+#include "Button.h"
+#include "ClientGameConsts.h"
+#include "CommonGameConsts.h"
+#include "Message.h"
+#include "ClientApp.h"
 
 void LobbyScene::OnInitialize()
 {
+	Button* PlayButton = CreateUIElement<Button>();
+	PlayButton->SetPosition({ WINDOW_WIDTH * 0.5f - BUTTON_WIDTH - 25.f, 75.f });
+	PlayButton->SetText("Play");
+	auto onPlayButtonClicked = [&]() {
+		Message message = Message::CreateMessage(MessageType::START_GAME, {});
+		I(ClientApp)->SendMessage(message);
+		message.content["id"] = I(ClientApp)->GetClientId();
+		};
+	PlayButton->BindOnClickFunction(onPlayButtonClicked);
+
+
+	Button* QuitButton = CreateUIElement<Button>();
+	QuitButton->SetPosition({ WINDOW_WIDTH * 0.5f + BUTTON_WIDTH * 2, 75.f });
+	QuitButton->SetText("Quit");
+	auto OnQuitButtonClicked = [&]() {
+		Message message = Message::CreateMessage(MessageType::LEAVE_LOBBY, {});
+		I(ClientApp)->SendMessage(message);
+		message.content["id"] = I(ClientApp)->GetClientId();
+		};
+	QuitButton->BindOnClickFunction(OnQuitButtonClicked);
 }
 
 void LobbyScene::OnUpdate(float deltaTime)
